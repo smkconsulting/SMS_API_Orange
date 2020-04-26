09ed052c796562ae3e61992060a6e4c75a9b2bbe
JackMutobu <jmutobu@outlook.com>
2020-04-26 21:23:14 +0300

    Modification

--- src/ConsoleTest/Program.cs
+++ src/ConsoleTest/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            var smsClient = SmsClient.Authenticate("Basic QmYwc0ZCOVdWUVJXZmR0Ynl6M0daRWY3RFdDajVyNkw6TjJyVEI2M0ZrVmU5NFJuMQ==").Result;
+            var smsClient = SmsClient.Authenticate("Token").Result;
             var response = smsClient.SendSms("Test", "243808790260", "243998954037", "Mandal").Result;
             if (response.IsSuccess)
                 Console.WriteLine(response.Value);
